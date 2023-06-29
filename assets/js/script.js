1149aa89a72ec7fc9dcf957069ca50b4a986edb7
Jarrod Blanning <jarrod.blanning@gmail.com>
2023-06-29 20:16:28 +1000

    API added for movie search. updated with correct update

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+var movieInput = document.getElementById('searchInput')
+var movieFormEl = document.getElementById('search-form')
+
+
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+
+    var movieName = movieInput.value.trim();
+    console.log(movieName)
+
+    if (movieName) {
+        getMovieApi(movieName);
+
+        movieInput.value = '';
+    } else {
+        alert('Please enter a movie name')
+    }
+}
+
+
+
+
+function getMovieApi(title) {
+    var requestMovieUrl = 'http://www.omdbapi.com/?t=' + title + '&apikey=2bd63a1d'
+
+    fetch(requestMovieUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data)
+    })
+}
+
+
+movieFormEl.addEventListener('submit', formSubmitHandler)
+
