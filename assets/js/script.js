9a34f286a2fc24166f637df9c6c0f61236191051
KalidNadere <131591052+KalidNadere@users.noreply.github.com>
2023-06-29 21:56:30 +1000

    added JS code to request trailer using fetch method & function to display trailer using iframe to embed content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,62 @@
+var movieInput = document.getElementById('searchInput')
+var movieFormEl = document.getElementById('search-form')
+var movieInfo = document.getElementById('movieInfo');
+var trailerContainer = document.getElementById('trailerContainer');
 
-// Function to display movie trailer, using iframe to embed content from youtube
-function showTrailer(videoId) {
-  var trailerContainer = document.getElementById('trailerContainer');
-  trailerContainer.innerHTML = `<iframe width='560' height='315' src='https://youtube.com/embed/${videoId}' frameborder='0' allowfullscreen></iframe>`;
+
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+
+    var movieName = movieInput.value.trim();
+    console.log(movieName)
+
+    if (movieName) {
+        getMovieApi(movieName);
+
+        movieInput.value = '';
+    } else {
+        alert('Please enter a movie name')
+    }
+}
+
+
+
+
+function getMovieApi(title) {
+    var requestMovieUrl = 'http://www.omdbapi.com/?t=' + title + '&apikey=2bd63a1d'
+
+    fetch(requestMovieUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data)
+    })
+}
+
+
+movieFormEl.addEventListener('submit', formSubmitHandler)
+
+
+
+// Fetch & display the movie trailer
+fetchTrailer(movieData.Title);
+
+// Using fetch method to request trailer from youtube using API key
+async function fetchTrailer(movieTitle) {
+  var apiKey = 'AIzaSyBLYYwlY0FawpIOHpAwRfHhh9nUa';
+  var apiUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(movieTitle + 'official trailer')}&maxResults=1&key=${apiKey}`;
+
+  try {
+    var response = await fetch(apiUrl);
+    var data = await response.json();
+    if (data.items && data.items.length > 0) {
+      var videoId = data.items[0].id.videoId;
+      showTrailer(videoId);
+    } else {
+      console.log('Trailer not found');
+    }
+  } catch (error) {
+    console.log('Error fetching trailer data:', error);
+  }
 }
